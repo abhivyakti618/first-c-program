cda0a26c56ef4353e852307b993a8c52cca72e0e
Abhivyakt <abhivyakti.618@gmail.com>
2026-09-13 10:49:16 +0530

    First commit

---
+++ Demo.cpp
@@ -0,0 +1,6 @@
+#include<iostream>
+using namespace std ;
+int main(){
+    cout<<"mera dimag kharab ho chuka hai mai satk chuki hun";
+    
+}
---
+++ areaofshphere.cpp
@@ -0,0 +1,7 @@
+include<iostream>
+using namespace std;
+int main(){
+    float r=9;
+    float volume =(4.2161*r*r*r);
+    cout<<volume;
+}
